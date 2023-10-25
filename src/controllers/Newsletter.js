dd959bdb3b3197b8c375d482b15c692a6d278e6b
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-10-25 23:08:56 +0300

    last touches on MVC for News and Newsletter with validation and documentation

--- src/controllers/Newsletter.js
+++ src/controllers/Newsletter.js
@@ -1,53 +1,100 @@
 import Newsletter from "../models/Newsletter.js";
 
-export const getAllNewsletters = async (req, res) =>{
-        try{
-            const newsletter = await Newsletter.find();
-            res.status(200).json(newsletter);
-        }
-        catch (error){
-            res.status(404).json({message: error.message});
+// Get all newsletters
+export const getAllNewsletters = async (req, res) => {
+  try {
+    // Retrieve all newsletters from the database
+    const newsletter = await Newsletter.find();
+    // Respond with the list of newsletters
+    res.status(200).json(newsletter);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
-        }
+export const getNewsletterById = async (req, res) => {
+  const { id } = res.body;
+  try {
+    // Validate and fetch a newsletter by its ID
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({
+          error: "News not found"
+        })
+      }
+    const newsletter = await Newsletter.findById(id);
+    // Respond with the retrieved newsletter
+    res.status(200).json(newsletter);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+
+// Add a new newsletter
+export const addNewsletter = async (req, res) => {
+  const { name } = req.body;
+  try {
+    // Create a new newsletter instance
+    const newNewsletter = new Newsletter({ name });
+    // Save the new newsletter to the database
+    await newNewsletter.save();
+    // Respond with the newly added newsletter
+    res.status(200).json(newNewsletter);
+  } catch (error) {
+    res.status(500).json({ error: "error adding newsletter" });
+    console.log(error);
+  }
+};
+
+
+// Delete a newsletter by ID
+export const deleteNewsletterById = async (req, res) => {
+  const { id } = req.body;
+  try {
+    // Validation for he type of the news ID
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({
+          error: "News not found"
+        })
+      }
+    // Attempt to delete a newsletter by its ID
+    const deletedNewsletter = await Newsletter.findByIdAndDelete(id);
+    if (!deletedNewsletter) {
+      // If the newsletter is not found, send an error response
+      return res.status(404).json({ message: "Newsletter not found" });
     }
+    // Respond with a success message if the newsletter is deleted
+    res.status(200).json({ message: `Newsletter deleted successfully` });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 
-export const getNewsletterById = async (req, res) =>{
-        const {id} = res.params;
-        try{
-            const newsletter = await Newsletter.findById(id);
-            res.status(200).json(newsletter);
-        }
-        catch(error){
-            res.status(404).json({message: error.message});
-        }
-    }
-
-
-export const addNewsletter = async (req , res) => {
-    const {name} = req.body 
+// Update newsletter emails by pushing a new email to the array
+export const updateNewsletterEmails = async (req, res) => {
+    const { id , email } = req.body;
     try {
-        const newNewsletter = new Newsletter({name})
-        await newNewsletter.save()
-        res.status(200).json(newNewsletter)
-    }catch (error) {
-        res.status(500).json({error : "error adding newsletter"})
-        console.log(error)
+      // Validate the newsletter ID
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({
+          error: "Newsletter not found"
+        });
+      }
+      // Find the newsletter by its ID
+      const newsletter = await Newsletter.findById(id);
+      if (!newsletter) {
+        return res.status(404).json({
+          error: "Newsletter not found"
+        });
+      }
+      // Push the new email into the subscribedUser array
+      newsletter.subscribedUser.push(email);
+      // Save the updated newsletter with the new email
+      await newsletter.save();
+      // Respond with the updated newsletter
+      res.status(200).json(newsletter);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
     }
-}
-
-
-export const deleteNewsletterById = async (req , res) => {
-    const {id}= req.params;
-    try{
-        const deletedNewsletter = await Newsletter.findByIdAndDelete(id)
-        if(!deletedNewsletter){
-            return res.status(404).json({message: "Newsletter not found"});
-        }
-        res.status(200).json({message: `Newsletter deleted successfully`});
-    }
-    catch(error){
-        res.status(404).json({message: error.message});
-
-    }
-}
+  };
