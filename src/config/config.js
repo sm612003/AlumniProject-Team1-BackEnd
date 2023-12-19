16f3ee8eab4bdb1f73c414b8153272eeb1bc84fc
souhad <souhadmoussa86@gmail.com>
2023-12-19 12:23:49 +0200

    changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,16 +1,33 @@
-import mongoose from "mongoose";
-import dotenv from 'dotenv'
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.USERNAME,
+  process.env.PASSWORD,
+  {
+    host: "db4free.net",
+    dialect: "mysql",
+  }
+);
 
-dotenv.config()
-const URL = process.env.URL 
-const PORT = process.env.PORT
+export const connect = async () => {
+  try {
+    await sequelize.authenticate({ force: true });
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.log("Unable to connect to database", error);
+  }
+};
 
-const connect = async () => {
-    try {
-      await mongoose.connect(URL);
-      console.log("Connected to mongoDB");
-    } catch (error) {
-      throw error;
-    }
-  };
-export default connect;
+export const syncronise = async () => {
+  try {
+    await sequelize.sync().then((result) => {
+      console.log(result);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default sequelize;
