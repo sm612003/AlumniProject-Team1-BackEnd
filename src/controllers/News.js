6b6b755c2b7c68f7f769bb8fd1a3b60aad4cffe9
jammal.souheir@gmail.com <jammal.souheir@gmail.com>
2023-12-19 15:43:28 +0200

    changing user schema

--- src/controllers/News.js
+++ src/controllers/News.js
@@ -8,57 +8,63 @@ import { log } from "console";
 
 // add news
 export const addNews = async (req, res) => {
-    try {
-     // Extract data from the request
+  try {
+    // Extract data from the request
     const {
       title,
       author,
-      description,
       date,
+      description,
       subtitle,
       subtitleDescription,
-      links,
-      Category,
-      newsletterID,
-    } = req.body; 
+      link,
+      categoryId,
+      newsletterId
+    } = req.body;
     const image = req.file?.path;
     // Validation , ensure all required fields are here
-    if (!title || !author || !description  || !Category || !newsletterID) {
+    if (!title || !author || !description || !categoryId || !newsletterId) {
       return res.status(400).json({ error: "Please provide all required data" });
     }
     // Fetch newsletter based on the provided ID
-    const newsletter = await Newsletter.findById(newsletterID)
+    const newsletter = await Newsletter.findById(newsletterId)
     if (!newsletter) {
       return res.status(404).json({ error: "Newsletter not found" });
     }
+     // Fetch category based on the provided ID
+     const category = await category.findById(categoryId)
+     if (!category) {
+       return res.status(404).json({ error: "category not found" });
+     }
     if (!req.file) {
       return res.status(400).json({ error: "Please upload an image" });
     }
-      // Create a new news post
-      const post = new News({
-        title: title,
-        author: author,
-        description: description,
-        date: date,
-        subtitle: subtitle,
-        subtitleDescription: subtitleDescription,
-        links: links,
-        image: image,
-        Category: Category,
-      });
-      // Save the news post and push it to the newsletter array
-      const addedNews = await post.save();
-      newsletter.news.push(post);
-      await newsletter.save()
-      // Respond with the newly created post
-      return res.status(200).json({
-        message : `News ${addedNews.title} is added successfuly`,
-        data : addedNews
-      });
-    } catch (error) {
-      return res.status(500).json({ message: error.message });
-    }
-  };
+    // Create a new news post
+    const post = new News({
+      title: title,
+      author: author,
+      description: description,
+      date: date,
+      subtitle: subtitle,
+      subtitleDescription: subtitleDescription,
+      link: link,
+      image: image,
+      categoryId: categoryId,
+      newsletterId:newsletterId
+    });
+    // Save the news post and push it to the newsletter array
+    const addedNews = await post.save();
+    newsletter.news.push(post);
+    await newsletter.save()
+    // Respond with the newly created post
+    return res.status(200).json({
+      message: `News ${addedNews.title} is added successfuly`,
+      data: addedNews
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 
 
@@ -67,9 +73,9 @@ export const getAllNews = async (req, res) => {
   try {
     // Fetch and sort all news posts by date
     const newsCard = await News.find().sort({ date: -1 });
-    if(!newsCard){
+    if (!newsCard) {
       return res.json({
-        error : "No News found"
+        error: "No News found"
       })
     }
     // Respond with the list of news posts
@@ -82,10 +88,10 @@ export const getAllNews = async (req, res) => {
 
 // see a new based on id
 export const getNewsById = async (req, res) => {
-  const id = req.params.id ;
+  const id = req.params.id;
   try {
     // Validation for he type of the news ID
-    if(!mongoose.Types.ObjectId.isValid(id)){
+    if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(404).json({
         error: "News not found"
       })
@@ -94,29 +100,29 @@ export const getNewsById = async (req, res) => {
     const newsCard = await News.findById(id);
     if (newsCard) {
       // Respond with the news post
-      res.status(200).json(newsCard); 
+      res.status(200).json(newsCard);
     } else {
       res.status(404).json({ message: "Blog not found" });
     }
   } catch (error) {
     res.status(500).json({ message: "Internal server error" });
   }
-}; 
+};
 
 
 // delete a news based on id
 export const deleteNews = async (req, res) => {
-  const id = req.body.id ;
+  const id = req.body.id;
   try {
     // Validation for he type of the news ID
-    if(!mongoose.Types.ObjectId.isValid(id)){
+    if (!mongoose.Types.ObjectId.isValid(id)) {
       return res.status(404).json({
         error: "News not found"
       })
     }
     // Delete the entire news post from database
-    const deletedNews = await News.findOneAndDelete({_id : id});
-      if (!deletedNews) {
+    const deletedNews = await News.findOneAndDelete({ _id: id });
+    if (!deletedNews) {
       return res.status(404).json({
         error: "Newscard is not found",
       });
@@ -134,7 +140,7 @@ export const deleteNews = async (req, res) => {
 export const updateNews = async (req, res) => {
   const id = req.body.id;
   // Validation for he type of the news ID
-  if(!mongoose.Types.ObjectId.isValid(id)){
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     return res.status(404).json({
       error: "News not found"
     })
@@ -142,65 +148,66 @@ export const updateNews = async (req, res) => {
   // Fetch the current news post
   const newsfirst = await News.findById(id)
   // Handle file upload and potential errors   
-    try {
-      // Extract updated data from the request
-      const {
-        author = newsfirst.author,
-        title = newsfirst.title ,
-        date = newsfirst.date ,
-        description = newsfirst.description ,
-        subtitle = newsfirst.subtitle , 
-        subtitleDescription = newsfirst.subtitleDescription,
-        links = newsfirst.links ,
-        Category = newsfirst.Category , 
-      } = req.body
-      const image = req.file?.path ;
-      // Update the news post and respond with the updated data
-      const news = await News.findByIdAndUpdate(
-        {_id: id},
-        {author: author ,
-        title: title ,
-        date : date ,
-        description : description,
-        subtitle: subtitle , 
+  try {
+    // Extract updated data from the request
+    const {
+      author = newsfirst.author,
+      title = newsfirst.title,
+      date = newsfirst.date,
+      description = newsfirst.description,
+      subtitle = newsfirst.subtitle,
+      subtitleDescription = newsfirst.subtitleDescription,
+      links = newsfirst.links,
+      Category = newsfirst.Category,
+    } = req.body
+    const image = req.file?.path;
+    // Update the news post and respond with the updated data
+    const news = await News.findByIdAndUpdate(
+      { _id: id },
+      {
+        author: author,
+        title: title,
+        date: date,
+        description: description,
+        subtitle: subtitle,
         subtitleDescription: subtitleDescription,
         links: links,
-        Category: Category ,
-        image : image
+        Category: Category,
+        image: image
       },
-        {new: true}
-      )
-      return res.json({
-        message : `News ${newsfirst.title} is updated successfuly`,
-        data: news
-      })   
-    } catch (error){
-    return res.status(500).json({
-      error : `Error, ${error.message}` 
+      { new: true }
+    )
+    return res.json({
+      message: `News ${newsfirst.title} is updated successfuly`,
+      data: news
     })
-    }   
-  };
+  } catch (error) {
+    return res.status(500).json({
+      error: `Error, ${error.message}`
+    })
+  }
+};
 
 
 // filter by categoryID 
-export const getNewsByCategory = async (req , res) => {
-  const {categoryName} = req.params
+export const getNewsByCategory = async (req, res) => {
+  const { categoryName } = req.params
   log(categoryName)
   try {
     // Find the category by name
-    const category = await Category.findOne({name : categoryName})
+    const category = await Category.findOne({ name: categoryName })
     console.log(category)
-    if(!category){
+    if (!category) {
       return res.status(404).json({
-        message:  'No category found'
+        message: 'No category found'
       })
     }
     // Find news posts that belong to the category and respond with them
-    const news = await News.find({ Category : category._id})
-    return res.status(200).json(news) 
-  } catch (error){
+    const news = await News.find({ Category: category._id })
+    return res.status(200).json(news)
+  } catch (error) {
     return res.status(500).json({
-      error : 'Error finding news'
+      error: 'Error finding news'
     })
   }
 }
