14c8433eb4e2c74c88d996d4d58873eb11228a00
souhad <souhadmoussa86@gmail.com>
2023-12-20 12:15:03 +0200

    edit the route

--- src/routes/Blogs.js
+++ src/routes/Blogs.js
@@ -1,14 +1,19 @@
 import express  from "express";
 import { getAllBlogs, getBlogById, deleteBlog, addBlog, updateBlog } from '../controllers/Blog.js'
 import { upload } from "../middlewares/multer.js";
+import { authenticateUser, authorizeUser } from "../middlewares/auth.js";
+
 
 const blogRouter= express.Router()
 
 blogRouter.get('/read/blogs',getAllBlogs);
 blogRouter.get('/read/blogsById/:id', getBlogById);
-blogRouter.post('/add/blogs',upload.single("image") , addBlog ) ;
-blogRouter.patch('/update/blogs',upload.single("image") , updateBlog);
-blogRouter.delete('/delete/blogs/:id', deleteBlog);
+blogRouter.post('/add/blogs',authenticateUser,
+authorizeUser(["Admin,user"]),upload.single("image") , addBlog ) ;
+blogRouter.patch('/update/blogs',authenticateUser,
+authorizeUser(["Admin,user"]),upload.single("image") , updateBlog);
+blogRouter.delete('/delete/blogs/:id',authenticateUser,
+authorizeUser(["Admin,user"]), deleteBlog);
 // blogRouter.get('/read/blogs/byCategory', getBlogsByCategory);
 
 
